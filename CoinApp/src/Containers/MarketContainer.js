76207e331e28822a772baeccb7ea6dae57496c01
Danh Tran <danhtrancao@gmail.com>
2022-01-23 20:35:53 +0700

    Enhance market screen

--- CoinApp/src/Containers/MarketContainer.js
+++ CoinApp/src/Containers/MarketContainer.js
@@ -1,23 +1,159 @@
-import React from 'react'
-import { Text, ScrollView } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  ActivityIndicator,
+  SafeAreaView,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import { getMarketList } from '@/Services/modules/api'
+import { EmptyListMessage, ItemSeparator, ListHeader } from '@/Components'
+import {
+  convertStringToCurrency,
+  convertStringToCurrencyDecimal,
+} from '@/Services/utils'
+import { t } from 'i18next'
 import { useTheme } from '@/Hooks'
 
-const MarketContainer = () => {
-  const { Fonts, Gutters, Layout } = useTheme()
+const MarketContainer = ({ navigation }) => {
+  const [marketCoins, setMarketCoinList] = useState([])
+  const [isLoading, setLoading] = useState(false)
+  const [isRefreshing, setRefresing] = useState(false)
+  const { List, Layout, Common } = useTheme()
+
+  useEffect(() => {
+    let isMounted = true
+    setLoading(true)
+
+    // Fetch market list for the first time
+    getMarketList().then(items => {
+      if (isMounted) {
+        setMarketCoinList(items)
+        setLoading(false)
+      }
+    })
+
+    // Fetch market list for the update latest informations (current price, ...)
+    const intervalId = setInterval(() => {
+      isMounted = true
+      getMarketList().then(items => {
+        if (isMounted) {
+          setMarketCoinList(items)
+        }
+      })
+    }, 20000)
+
+    return () => {
+      clearInterval(intervalId)
+      isMounted = false
+    }
+  }, [])
+
+  const onNavigateSearchContainer = () => {
+    navigation.navigate('Search')
+  }
+
+  const onRefresh = () => {
+    let mounted = true
+    setRefresing(true)
+    getMarketList().then(items => {
+      if (mounted) {
+        setMarketCoinList(items)
+        setRefresing(false)
+      }
+    })
+    return () => {
+      mounted = false
+    }
+  }
+
+  const ItemDetailView = ({ item }) => {
+    return (
+      <View>
+        <TouchableOpacity onPress={() => navigateToCoinDetail(item)}>
+          <View style={List.itemDetailViewStyle}>
+            <View style={Layout.noFlex}>
+              <Text style={List.itemNoStyle}>{item.market_cap_rank}</Text>
+            </View>
+            <View style={[List.itemCoinViewStyle, Layout.coinFlex]}>
+              <Image
+                style={List.tinyLogo}
+                source={{
+                  uri: item.image,
+                }}
+              />
+              <Text numberOfLines={1} style={List.itemStyle}>
+                {item.symbol.toUpperCase()}
+              </Text>
+            </View>
+            <View style={Layout.priceFlex}>
+              <Text style={List.itemStyle}>
+                {convertStringToCurrencyDecimal(item.current_price)}
+              </Text>
+            </View>
+            <View style={Layout.durationFlex}>
+              {item.price_change_percentage_24h.toFixed(2) >= 0 ? (
+                <Text style={List.itemIncreaseStyle}>
+                  {item.price_change_percentage_24h.toFixed(2)}%
+                </Text>
+              ) : (
+                <Text style={List.itemDecreaseStyle}>
+                  {item.price_change_percentage_24h.toFixed(2)}%
+                </Text>
+              )}
+            </View>
+            <View style={Layout.marketCapFlex}>
+              <Text style={List.itemStyle}>
+                {convertStringToCurrency(item.market_cap)}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  const navigateToCoinDetail = item => {
+    navigation.navigate('CoinDetail', {
+      id: item.id,
+    })
+  }
 
   return (
-    <ScrollView
-      style={Layout.fill}
-      contentContainerStyle={[
-        Layout.fill,
-        Layout.colCenter,
-        Gutters.smallHPadding,
-      ]}
-    >
-      <Text style={[Layout.fill, Fonts.textCenter, Fonts.textSmall]}>
-        Market
-      </Text>
-    </ScrollView>
+    <SafeAreaView>
+      <View style={Common.titleViewStyle}>
+        <Text style={Common.titleTextViewStyle}>{t('name')}</Text>
+        <TouchableOpacity
+          onPress={async () => await onNavigateSearchContainer()}
+        >
+          <Icon
+            style={Layout.twoFlex}
+            size={25}
+            name="search-outline"
+            color="#887700"
+          />
+        </TouchableOpacity>
+      </View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={marketCoins}
+          stickyHeaderIndices={[0]}
+          keyExtractor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparator}
+          ListHeaderComponent={ListHeader}
+          renderItem={ItemDetailView}
+          ListEmptyComponent={EmptyListMessage}
+          onRefresh={onRefresh}
+          refreshing={isRefreshing}
+          contentContainerStyle={{ paddingBottom: 50 }}
+        />
+      )}
+    </SafeAreaView>
   )
 }
 
